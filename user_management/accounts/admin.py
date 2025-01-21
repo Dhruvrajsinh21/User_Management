b2c782d2815de31d2b0a5f63ce355a40a3f9b9f3
Dhruvrajsinh Solanki <dhruvrajsinh251103@gmail.com>
2025-01-21 08:09:12 +0530

    Updated

--- user_management/accounts/admin.py
+++ user_management/accounts/admin.py
@@ -3,20 +3,14 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    # Fields to display in the admin list view
     list_display = ('id', 'name', 'email', 'phone_number', 'is_active', 'is_super_admin')
-    # Fields to filter in the admin list view
     list_filter = ('is_active', 'is_super_admin')
-    # Fields to search in the admin list view
     search_fields = ('name', 'email', 'phone_number')
-    # Fields to sort in the admin list view
     ordering = ('-id',)
-    # Fields to display in the edit view
     fieldsets = (
         (None, {'fields': ('name', 'email', 'password', 'phone_number')}),
         ('Permissions', {'fields': ('is_active', 'is_super_admin')}),
     )
-    # Fields to display when creating a new user in admin
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
--- user_management/accounts/models.py
+++ user_management/accounts/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)  # Store hashed password
+    password = models.CharField(max_length=255) 
     phone_number = models.CharField(max_length=15, unique=True)
-    is_active = models.BooleanField(default=True)  # For account deactivation
+    is_active = models.BooleanField(default=True) 
     is_super_admin = models.BooleanField(default=False)
 
     def __str__(self):
